2ae0eeea7e5a646ed8f6c387d5bb39d6a5cadcb3
VictorAGuerra <victoraguerra1302@gmail.com>
2025-11-26 14:31:37 -0300

    conexão wifi + arduino cloud

--- SIGMA_nov18a.ino
+++ SIGMA_nov18a.ino
@@ -1,40 +1,40 @@
+#include "thingProperties.h"
+
 #define LDR 15
 #define LED_STRONG 23
 #define LED_DIM 21
-#define IRS 22
-
-int light;
-int presence;
+#define IRS 19
 
 void setup() {
   Serial.begin(9600);
+  delay(1500);
+
   pinMode(LED_STRONG, OUTPUT);
   pinMode(LED_DIM, OUTPUT);
   pinMode(LDR, INPUT);
   pinMode(IRS, INPUT);
+
+  initProperties();
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
 }
 
 void loop() {
+  ArduinoCloud.update();
+
   light = analogRead(LDR);
-  presence = !(digitalRead(IRS));
-  
   Serial.println(light);
-  Serial.println(presence);
-  
+  presence = !digitalRead(IRS);
+
   if (light < 400 && presence == LOW) {
     digitalWrite(LED_STRONG, LOW);
     digitalWrite(LED_DIM, HIGH);
-  }
-
-  else if (light < 400 && presence == HIGH) {
+  } else if (light < 400 && presence == HIGH) {
     digitalWrite(LED_DIM, LOW);
     digitalWrite(LED_STRONG, HIGH);
-  }
-
-  else{
-    digitalWrite(LED_STRONG, LOW);
+  } else {
+    digitalWrite(LED_STRONG, LOW);  
     digitalWrite(LED_DIM, LOW);
   }
-  
-  delay(100);
+
+  delay(500);
 }
